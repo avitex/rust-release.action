11fe3cd4cdb97224d37b334b75172b6e94788cdd
avitex <theavitex@gmail.com>
2021-12-30 01:14:34 +1100

    add archiving

--- index.js
+++ index.js
@@ -3,85 +3,48 @@ import * as github from '@actions/github';
 import { single as format } from 'paraphrase';
 
 import { build } from './lib/build.js';
+import { archiveAssets, validateTypes } from './lib/archive.js';
 import { uploadAsset } from './lib/upload.js';
 
-// function archiveTarGzip(files, output) {
-//   await tar.c(
-//     {
-//       gzip: true,
-//       file: output,
-//     },
-//     files
-//   )
-// }
-
-function getArchiveInput() {
-  const types = core.getInput('archive');
-  const include = core.getMultilineInput('archive_include');
-  const options = {};
-
-  if (types != '') {
-    options.types = types.split(',');
-  }
-  if (include != '') {
-    options.include = include.split('\n');
-  }
-  
-  return options;
-}
-
-function getAssetInput() {
-  const name = core.getInput('asset');
-  const format = core.getInput('asset_format');
-  const digest = core.getBooleanInput('asset_digest');
-  const options = { digest };
-
-  if (name != '') {
-    options.name = name;
-  }
-  if (format != '') {
-    options.format = format;
-  }
-
-  return options;
-}
-
 function getInputs() {
-  const asset = getAssetInput();
-  const archive = getArchiveInput();
-  const pkg = core.getInput('pkg');
-  const target = core.getInput('target');
-  const binary = core.getInput('binary');
-  const profile = core.getInput('profile');
-  const githubToken = core.getInput('token');
-
-  return {
-    pkg,
-    asset,
-    archive,
-    target,
-    binary,
-    profile,
-    githubToken,
+  const inputs = {
+    pkg: core.getInput('pkg'),
+    target: core.getInput('target'),
+    binary: core.getInput('binary'),
+    profile: core.getInput('profile'),
+    githubToken: core.getInput('token'),
+    assetName: core.getInput('asset'),
+    assetFormat: core.getInput('asset_format'),
+    assetDigest: core.getBooleanInput('asset_digest'),
+    archive: core.getBooleanInput('archive'),
+    archiveTypes: [],
+    archiveInclude: core.getMultilineInput('archive_include'),
   };
+
+  const archiveTypes = core.getInput('archive_types');
+  if (archiveTypes) {
+    inputs.archiveTypes = archiveTypes.split(',');
+    validateTypes(inputs.archiveTypes);
+  }
+
+  return inputs;
 }
 
 try {
+  const inputs = getInputs();
+
   // https://docs.github.com/en/developers/webhooks-and-events/webhooks/webhook-events-and-payloads#release
   const repo = github.context.repo;
   const release = github.context.payload['release'];
-  const inputs = getInputs();
 
   // Only run if a draft is saved, or a release or pre-release is published
   // without previously being saved as a draft.
-  if (github.context.payload.action != 'created' || release == undefined) {
-    process.exit(0);
+  if (github.context.payload.action != 'created' || !release) {
+    throw new Error('Requires a created release context');
   }
-  
-  const octokit = github.getOctokit(inputs.githubToken);
 
   core.startGroup('Build binary');
-  const { output, binary, profile, target, extension } = build({
+  const { binary, binaryPath, binaryExtension, profile, target, isWindowsTarget } = build({
     pkg: inputs.pkg,
     binary: inputs.binary,
     profile: inputs.profile,
@@ -90,38 +53,56 @@ try {
   });
   core.endGroup();
 
-  const asset = inputs.asset;
+  const { archive, archiveInclude, archiveTypes } = inputs;
+  const octokit = github.getOctokit(inputs.githubToken);
+  const assetFormatArgs = {
+    repo,
+    binary,
+    target,
+    release,
+    profile,
+  };
 
-  if (!asset.name) {
-    if (asset.format) {
-        const formatArgs = {
-            repo,
-            asset,
-            binary,
-            target,
-            release,
-            profile,
-            extension,
-        };
-        asset.name = format(asset.format, formatArgs);
-    } else {
-        asset.name = binary.name;
+  const assets = [];
+
+  if (archive) {
+    core.startGroup('Archive release assets');
+
+    if (archiveTypes.length == 0) {
+      if (isWindowsTarget) {
+        archiveTypes.push('zip');
+      } else {
+        archiveTypes.push('tar.gz');
+      }
     }
+
+    for (const type of archiveTypes) {
+      assets.push(archiveAssets(type, binaryPath, archiveInclude));
+    }
+
+    core.endGroup();
+  } else {
+    assets.push({
+      path: binaryPath,
+      extension: binaryExtension,
+    });
   }
 
-  // TODO: archive
-  // for(const asset of output.assets) {
-  //   let assetPath = asset.path;
-
-  //   if (archiveTypes.length > 0) {
-      
-  //   } else {
-  //     uploadAsset(octokit, github.context.repo, release, asset.path, asset.name, assetDigest)
-  //   }
-  // }
-
   core.startGroup('Upload release assets');
-  await uploadAsset(octokit, github.context.repo, release, output, asset.name, asset.digest);
+  for (const asset of assets) {
+    if (inputs.assetName) {
+      asset.name = `${inputs.assetName}${asset.extension}`;
+    } else if (inputs.assetFormat) {
+      asset.name = format(inputs.assetFormat, {
+        ...assetFormatArgs,
+        extension: asset.extension
+      });
+    } else {
+      asset.name = `${binary.name}${asset.extension}`;
+    }
+
+    await uploadAsset(octokit, repo, release, asset.path, asset.name, inputs.assetDigest);
+  }
   core.endGroup();
 
 } catch (error) {
